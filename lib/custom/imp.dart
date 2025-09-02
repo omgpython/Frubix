24d5a2166e2b61a83ceb7dfd364876994773183a
Harshal Jariwala <jariwalaharshal64@gmail.com>
2025-09-03 00:01:16 +0530

    init

--- lib/custom/imp.dart
+++
@@ -1,9 +0,0 @@
-// Future<String> pickImage({required BuildContext context}) async {
-//   final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
-//   if (pickedFile != null) {
-//     return base64Encode(await pickedFile.readAsBytes());
-//   } else {
-//     Alert(context: context, message: 'No Image Selected', type: 'warning');
-//     return 'NONE';
-//   }
-// }
